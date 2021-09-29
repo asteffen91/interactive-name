4fda50d6f0d6a908f62268c08f1142830d265750
Austin <austin.steffen@colorado.edu>
2021-09-29 10:44:38 -0600

    First commit

---
+++ Particle.js
@@ -0,0 +1,46 @@
+class Particle {
+    constructor(x, y, x0, y0) {
+        // Kinematics properties
+        this.pos = new p5.Vector(x, y);
+        this.vel = new p5.Vector(0, 0);
+        this.acc = new p5.Vector(0, 0);
+        this.mass = 1;
+
+        // Attract and repel
+        this.desire = new p5.Vector(x0, y0);
+
+        // Render properties
+        this.r = 10;
+        this.color = random(colorPallet.slice(1));
+    }
+
+    draw() {
+        noStroke();
+        fill(this.color);
+        circle(this.pos.x, this.pos.y, this.r);
+    }
+
+    applyForce(x, y) {
+        let force = new p5.Vector(x, y);
+        this.acc = force.div(this.mass);
+        this.vel.add(this.acc);
+        this.pos.add(this.vel);
+    }
+
+    update() {
+        let mousePos = createVector(mouseX, mouseY);
+
+        let force1 = new p5.Vector(0, 0);
+        if (p5.Vector.dist(this.pos, mousePos) < 200) {
+            let d = constrain(p5.Vector.dist(this.pos, mousePos), 1, 10000);
+            force1 = p5.Vector.sub(this.pos, mousePos).normalize().div(d).mult(100.0);
+        }
+
+        let force2 = p5.Vector.sub(this.pos, this.desire).mult(-0.01);
+        let force3 = p5.Vector.mult(this.vel, -0.1);
+        let force = p5.Vector.add(force1, force2);
+        force = force.add(force3);
+        this.applyForce(force.x, force.y);
+        this.draw();
+    }
+}
---
+++ sketch.js
@@ -0,0 +1,21 @@
+let p = [];
+const N = 1000;
+
+const colorPallet = ['#343233', '#B6C8A9', '#C8EAD3', '#CFFFE5', '#CEDADA'];
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(colorPallet[0]);
+  if (p.length < N) {
+    for (let i = 0; i < N / 50; i++) {
+      p.push(new Particle(width / 2, height / 2, random(width), random(height)));
+    }
+  }
+
+  for (let j = 0; j < p.length; j++) {
+    p[j].update();
+  }
+}

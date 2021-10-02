dcba16ef2f078fb14e1b2eda3443919b49db9537
Austin <austin.steffen@colorado.edu>
2021-10-01 22:41:17 -0600

    Center text

--- sketch.js
+++ sketch.js
@@ -1,5 +1,7 @@
 let p = [];
 const N = 1000;
+let points;
+let bounds;
 
 const colorPallet = ['#343233', '#B6C8A9', '#C8EAD3', '#CFFFE5', '#CEDADA'];
 
@@ -9,13 +11,18 @@ function preload() {
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  points = font.textToPoints('Austin', width * 0.05, height / 2, 400, {
+
+  bounds = font.textBounds(' Austin ', 0, 0, 400);
+  points = font.textToPoints(' Austin ', 0, 0, 400, {
     sampleFactor: 0.1,
     simplifyThreshold: 0
   });
 
+  let sf = width / bounds.w;
+  let vo = height / 2 + bounds.h / 2;
+
   for (let i = 0; i < points.length; i++) {
-    p.push(new Particle(width / 2, height / 2, points[i].x, points[i].y));
+    p.push(new Particle(width / 2, height / 2, points[i].x * sf, points[i].y * sf + vo));
   }
 }
 
